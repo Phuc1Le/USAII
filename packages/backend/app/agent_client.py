6f6ca29a426bba90bf1ea1ef4ae64ea253c2c6f1
Le Hong Phuc <lehongphucnasa@gmail.com>
2026-06-23 05:53:10 +0700

    done phuc

--- packages/backend/app/agent_client.py
+++ packages/backend/app/agent_client.py
@@ -3,7 +3,9 @@
 import os
 import httpx
 from app import schemas
+from dotenv import load_dotenv
 
+load_dotenv()
 AGENT_URL = os.environ.get("AGENT_URL", "http://localhost:8001")
 USE_MOCK_AGENT = os.environ.get("USE_MOCK_AGENT", "").lower() == "true"
 
